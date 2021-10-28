4bda0e1acc4c5f2fb96d53e5ca2639f99f3d4307
luopeng <15158074877@163.com>
2021-10-28 22:11:54 +0800

    migrate to android embedding v2

--- android/src/main/java/com/startupcloud/aes/flutter_aes/FlutterAesPlugin.java
+++ android/src/main/java/com/startupcloud/aes/flutter_aes/FlutterAesPlugin.java
@@ -1,17 +1,27 @@
 package com.startupcloud.aes.flutter_aes;
 
+import androidx.annotation.NonNull;
+
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
 
 /** FlutterAesPlugin */
 public class FlutterAesPlugin implements MethodCallHandler {
   /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_aes");
-    channel.setMethodCallHandler(new FlutterAesPlugin());
+  private MethodChannel channel;
+
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "flutter_aes");
+    channel.setMethodCallHandler(this);
+  }
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    channel.setMethodCallHandler(null);
   }
 
   @Override
